728ee2d2667830003c706db49dcd4aa5a11cd205
APATAKER <therealthelip@gmail.com>
2024-12-20 01:51:10 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -15,17 +15,17 @@ http.createServer(function (req, res) {
   if (err) throw err;
   console.log('File deleted!');
 });
-  //fs.readFile('index.html', function(err, data) {
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.write(data);
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
     //return res.end();
     //res.writeHead(200, {'Content-Type': 'text/html'});
-    //var q = url.parse(req.url, true).query;
-    //var txt = q.year + " " + q.month;
-    //res.write("The date and time are currently: " + dt.myDateTime());
-    //res.write(req.url);
-    //res.end(txt);
-  //});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write(req.url);
+    res.end(txt);
+  });
   console.log(qa.host);
   console.log(qa.pathname);
   console.log(qa.search);
