3dae9d0afb23ad24930b8b32b4186197633bb444
APATAKER <therealthelip@gmail.com>
2024-12-20 03:40:13 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -45,10 +45,26 @@ http.createServer(function (req, res) {
     eventEmitter.emit('scream');
     res.end();
   });
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
+  
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = '/home/ec2-user/' + files.filetoupload.originalFilename;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
   console.log(qa.host);
   console.log(qa.pathname);
   console.log(qa.search);
