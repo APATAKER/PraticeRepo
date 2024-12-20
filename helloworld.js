628a1504f9c2656194e8d70643c7abaf5ad88b3f
APATAKER <therealthelip@gmail.com>
2024-12-20 01:59:42 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -7,20 +7,25 @@ var qa = url.parse(adr, true);
 var qdata = qa.query;
 
 http.createServer(function (req, res) {
-  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
+  //fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  //if (err) throw err;
+  //console.log('Saved!');
+//});
   fs.unlink('mynewfile1.txt', function (err) {
   if (err) throw err;
   console.log('File deleted!');
 });
-  fs.readFile('index.html', function(err, data) {
+  var q = url.parse(req.url, true).query;
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     //return res.end();
     //res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.write("The date and time are currently: " + dt.myDateTime());
     res.write(req.url);
