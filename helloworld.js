203c6b88e0937297266d41c83dded5f54bc675b4
APATAKER <therealthelip@gmail.com>
2024-12-20 00:27:23 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var dt = require('./dateMod');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.write(req.url);
-  res.end(txt);
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+  //res.writeHead(200, {'Content-Type': 'text/html'});
+ //var q = url.parse(req.url, true).query;
+  //var txt = q.year + " " + q.month;
+ // res.write("The date and time are currently: " + dt.myDateTime());
+ //res.write(req.url);
+  //res.end(txt);
 }).listen(8081);
