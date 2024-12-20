e6c52dd4d5504a66696d063ece8be8b8d5ff0134
APATAKER <therealthelip@gmail.com>
2024-12-20 00:18:33 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -5,5 +5,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
   res.write(req.url);
-  res.end('Hello World!');
+  res.end();
 }).listen(8081);
