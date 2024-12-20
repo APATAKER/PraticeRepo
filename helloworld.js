ee578bc018a4b661f21ea10cefe3546138e8f871
APATAKER <therealthelip@gmail.com>
2024-12-20 00:39:58 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -4,6 +4,10 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
